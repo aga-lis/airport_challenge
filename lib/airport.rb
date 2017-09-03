661df1893c963fecec75c03e213dbc8448eea318
Agnieszka Lis <agnieszkalis@gmail.com>
2017-09-03 18:11:48 +0100

    Added error message when airport full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,17 @@
 class Airport
+  def initialize(capacity)
+    @capacity = capacity
+    @planes = []
+  end
+
   def land(plane)
+    raise "Airport full, can't land" if @planes.size >= @capacity
+    @planes << plane
   end
 
   def take_off(plane)
+  end
+
+  def full?
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,18 @@
 require 'airport'
 describe Airport do
-  subject(:airport) { described_class.new }
+  subject(:airport) { described_class.new(100) }
   it 'tells the plane to land' do
     expect(airport).to respond_to(:land).with(1).argument
   end
   it 'tells the plane to take off' do
     expect(airport).to respond_to(:take_off).with(1).argument
+  end
+  it 'does not allow planes to land when full' do
+    expect(airport).to respond_to(:full?)
+    airport = Airport.new(100)
+    100.times do
+      airport.land(:plane)
+    end
+    expect { airport.land(:plane) }.to raise_error "Airport full, can't land"
   end
 end
